01910c2325a05773b401485a69f30c17544924c3
Dorsa Eshraghi Boroojeni <vhappyvsad@gmail.com>
2018-04-26 11:24:29 -0700

    comments

--- game/game.py
+++ game/game.py
@@ -1,28 +1,31 @@
 import random
 
-val = ['r', 'p', 's']
 
+#accepted input values (user choice)
+val = ['r', 'p', 's']
+#validate user input
 def valid(ans):
     if ans not in val:
         return False
     else:
         return True
 
+# _u stands for you and _c stands for computer choice
 def play(_u,_c):
 
-    win = {'r':'s','p':'r','s':'p'}
+
     if _u == _c:
         return 'again'
     elif win[_u] == _c:
         return 'Won'
     else:
         return 'Lost'
-
+#comuter choice is a random value between 0 - 1 -2
 def c():
     _rand = random.randint(0, 2)
     _c = val[_rand]
     return _c
-
+#continue the game until someone wins
 if __name__ == "__main__":
     res = 'again'
     while res == 'again':
